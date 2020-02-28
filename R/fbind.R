9876946bc31be4b6842088510a6455b080f2cf24
Karanpal Singh <karanpal.singh@alumni.ubc.ca>
2020-02-27 22:24:32 -0800

    R Package

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,12 @@
+#' fbind
+#'
+#' @param a
+#' @param b
+#'
+#' @return concat factor
+#' @export
+#'
+#' @examples
 fbind <- function(a,b){
        factor(c(as.character(a),as.character(b)))
   }
---
+++ tests/testthat.R
@@ -0,0 +1,14 @@
+library(testthat)
+library(exerciseR)
+
+test_check("exerciseR")
+
+test_that("fbind() binds factor (or character)", {
+  x <- c("a", "b")
+  x_fact <- factor(x)
+  y <- c("c", "d")
+  z <- factor(c("a", "b", "c", "d"))
+
+  expect_identical(fbind(x, y), z)
+  expect_identical(fbind(x_fact, y), z)
+})
